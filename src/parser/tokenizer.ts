4db1d1b9c2a058f455b5707ae9e7dd7cfb8473cd
Drincann <1019933576@qq.com>
2022-07-15 21:01:16 +0800

    fix: 修复用于 tokenize 的状态机在 number token 结束时忽略一个 right paren token 的问题

--- src/parser/tokenizer.ts
+++ src/parser/tokenizer.ts
@@ -1,18 +1,18 @@
 import { isNameChar, isNumberChar } from "../helper";
 
-interface ParenToken {
+export interface ParenToken {
   type: 'paren';
   value: '(' | ')';
 }
-interface NumberToken {
+export interface NumberToken {
   type: 'number';
   value: `${number}`;
 }
-interface StringToken {
+export interface StringToken {
   type: 'string';
   value: `"${string}"`;
 }
-interface NameToken {
+export interface NameToken {
   type: 'name';
   value: string;
 }
@@ -23,19 +23,26 @@ export type Token =
   | StringToken
   | NameToken;
 
-type StateMachine = (input: string) => Token | null;
-const genStateMachine = (): StateMachine => {
+type StateMachine = () => ({ done: boolean, token: Token | null });
+
+const genStateMachine = (input: string): StateMachine => {
   const NONE = 0, STRING = 1, NUMBER = 2, NAME = 3;
   let state: number = NONE;
   let value = '';
-  return (char): Token | null => {
+  let cursor = 0;
+  return (): { done: boolean, token: Token | null } => {
+    const char = input[cursor];
     switch (state) {
       case NONE:
+        ++cursor;
         switch (true) {
           case char === '\n' || char === ' ':
             break;
           case char === '(' || char === ')': // end of the paren
-            return { type: 'paren', value: char } as ParenToken;
+            return {
+              done: cursor >= input.length,
+              token: { type: 'paren', value: char } as ParenToken,
+            };
           case isNumberChar(char) === true: // start of the number
             state = NUMBER;
             value = char;
@@ -54,11 +61,15 @@ const genStateMachine = (): StateMachine => {
         break;
 
       case STRING:
+        ++cursor;
         switch (char) {
           case '"': // end of the string
             state = NONE;
             value += '"';
-            return { type: 'string', value: value } as StringToken;
+            return {
+              done: cursor >= input.length,
+              token: { type: 'string', value: value } as StringToken,
+            };
           default:
             value += char;
             break;
@@ -68,11 +79,15 @@ const genStateMachine = (): StateMachine => {
       case NUMBER:
         switch (true) {
           case isNumberChar(char): // in number
+            ++cursor;
             value += char;
             break;
           default: // end of the number
             state = NONE;
-            return { type: 'number', value: value } as NumberToken;
+            return {
+              done: cursor >= input.length,
+              token: { type: 'number', value: value } as NumberToken,
+            };
         }
         break;
 
@@ -80,25 +95,32 @@ const genStateMachine = (): StateMachine => {
         switch (true) {
           case char === '\n' || char === ' ':
             state = NONE;
-            return { type: 'name', value: value } as NameToken;
+            return {
+              done: cursor >= input.length,
+              token: { type: 'name', value: value } as NameToken,
+            };
           case isNameChar(char):
+            ++cursor;
             value += char;
             break;
           default:
             throw new Error(`no such name value char: ${char}`);
         }
     }
-    return null;
+    return {
+      done: cursor >= input.length,
+      token: null,
+    };
   }
 }
 
 export const tokenize = (input: string) => {
   const tokens: Token[] = [];
-  const step = genStateMachine();
-  for (const char of input) {
-    const token = step(char);
+  const step = genStateMachine(input);
+  do {
+    var { done, token } = step();
     if (token) tokens.push(token);
-  }
+  } while (!done);
   return tokens;
 };
 
