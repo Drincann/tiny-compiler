c32f246272dcff833b8ddab89c64d2fbd2c8e070
Drincann <1019933576@qq.com>
2022-07-19 14:22:29 +0800

    refactor: 为 transformer 添加注释

--- src/transformer/transformer.ts
+++ src/transformer/transformer.ts
@@ -35,11 +35,23 @@ interface IdentifierLiteralASTNode {
 }
 
 type Visitor = (node: LispASTNode, parentNode: LispASTNode, transformedParentNode: CLickASTNode) => CLickASTNode;
+
+// traverse the non-leaf nodes
 const traverse = (ast: LispASTNode, transformedAst: CLickASTNode, visit: Visitor): void => {
   switch (ast.type) {
     case 'Program':
       ast.body.forEach(node => {
-        traverse(node, visit(node, ast, transformedAst), visit);
+        traverse(
+          /* node -> lower parent node */
+          node,
+          /* transformedAst -> 
+            visitor will return a new node transformed from the parent node
+            this new node is the root of the transformed AST
+          */
+          visit(node, ast, transformedAst),
+          /* visit -> traverse functino */
+          visit
+        );
       });
       break;
     case 'CallExpression':
@@ -49,7 +61,7 @@ const traverse = (ast: LispASTNode, transformedAst: CLickASTNode, visit: Visitor
       break;
     case 'NumberLiteral':
     case 'StringLiteral':
-      // visit(ast, null);
+      // this is a leaf node
       break;
     default:
       throw new TypeError(`Unknown AST node type: ${(ast as LispASTNode)?.type}`);
@@ -62,12 +74,13 @@ export const transform = (ast: LispAST): CLickAST => {
     body: [],
   };
 
-
   traverse(ast, transformed, (node, parentNode, transformedParentNode) => {
     switch (node.type) {
       case 'CallExpression':
         let expression: ExpressionStatementASTNode | CallExpressionASTNode | null = null;
         if (parentNode.type === 'Program') {
+          // ExpressionStatement only exists in Program node in our compiler
+          // so for the CallExpression node from the Lisp AST, wo neet to wrap it in an ExpressionStatement
           expression = {
             type: 'ExpressionStatement',
             expression: {
@@ -95,16 +108,17 @@ export const transform = (ast: LispAST): CLickAST => {
         }
         throw new TypeError(`CallExpression literal not allowed in ${parentNode.type}`);
       case 'StringLiteral':
-        const clikeStrNode: StringLiteralASTNode = {
+        const clikeNode: StringLiteralASTNode = {
           type: 'StringLiteral',
           value: node.value,
         };
+        // leaf node is possibly in ExpressionStatement or CallExpression
         if (transformedParentNode.type === 'ExpressionStatement') {
-          transformedParentNode.expression.arguments.push(clikeStrNode);
-          return clikeStrNode;
+          transformedParentNode.expression.arguments.push(clikeNode);
+          return clikeNode;
         } else if (transformedParentNode.type === 'CallExpression') {
-          transformedParentNode.arguments.push(clikeStrNode);
-          return clikeStrNode;
+          transformedParentNode.arguments.push(clikeNode);
+          return clikeNode;
         }
         throw new TypeError(`String literal not allowed in ${transformedParentNode.type}`);
       case 'NumberLiteral':
